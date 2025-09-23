c9f168748779932210fb4b7c44da74aeff8029fe
A-007481D <labidabdelmalek@gmail.com>
2025-09-23 11:11:53 +0100

    feat(DAO): create the generic DAO interface that will be inherited by all the other DAO Interfaces

--- src/dao/GenericDAO.java
+++ src/dao/GenericDAO.java
@@ -1,4 +1,11 @@
 package dao;
 
-public class GenericDAO {
+import java.util.List;
+import java.util.Optional;
+
+public interface GenericDAO<T> {
+    T save(T t);
+    Optional<T> findById(Long id);
+    List<T> findAll();
+    boolean deleteById(Long id);
 }
