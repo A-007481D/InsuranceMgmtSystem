359b58917c1f18d5afda04ed5fe34ba112e9f35b
A-007481D <labidabdelmalek@gmail.com>
2025-09-23 10:57:14 +0100

    feat(model): update the model class Conseiller

---
+++ src/model/Conseiller.java
@@ -0,0 +1,11 @@
+package model;
+
+
+public class Conseiller extends Person {
+    public Conseiller() {
+        super();
+    }
+    public Conseiller(Long id, String nom, String prenom, String email) {
+        super(id, nom, prenom, email);
+    }
+}
