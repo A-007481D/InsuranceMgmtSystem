45b61fd8332471abe0432c810b445d4bba07a8ec
A-007481D <labidabdelmalek@gmail.com>
2025-09-25 21:57:39 +0100

    feat(views): create the main entry point

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
-public class Main{
+
+
+import view.MainView;
+
+
+public class Main {
     public static void main(String[] args) {
-        System.out.print("hello world");
+        MainView.showMainMenu();
     }
 }
